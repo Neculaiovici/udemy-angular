df7a48797c23269bbbdf592219a196357cd8dbe1
INTEGRISOFT\robert.mocanu <robert.mocanu@integrisoft.ro>
2024-03-13 16:29:58 +0200

    complete for the moment

--- shopping/src/app/shopping-list/shopping-list.component.ts
+++ shopping/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Shugar', 3),
+    new Ingredient('Floor', 5),
+    new Ingredient('Apple', 2),
+    new Ingredient('Tomatos', 12),
+  ];
 
   constructor() { }
 
